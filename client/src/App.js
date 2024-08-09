3db5004b552d23ae334d9f5d02d20c02cae6e7d6
Ryu <glukec4@gmail.com>
2024-08-09 12:50:49 +0200

    basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader.js';
 
 function App() {
+  const [showWebsite, setShowWebsite] = useState(true);
+
+  useEffect(() => {
+    setTimeout(()=> setShowWebsite(true), 5000);
+    
+  }, [])
+
+  if (showWebsite)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.left}>
+        <div className={styles.placeholder}>placeholder</div>
+      </div>
+      <div className={styles.middle}></div>
+      <div className={styles.right}>
+        <div className={styles.filter}></div>
+        <div className={styles.navi}></div>
+      </div>
     </div>
   );
+  else return <Loader />;
 }
 
 export default App;
---
+++ client/src/components/Loader.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import styles from './Loader.module.scss';
+
+function Loader({  }) {
+
+  return (
+    <div className={styles.Loader}>
+      <div className={styles["middle-wrapper"]}>
+        <div className={styles["puzzle-cube"]} ><div></div><div></div></div>
+        <div className={styles["puzzle-cube"]} ><div></div><div></div></div>
+        <div className={styles["puzzle-cube"]} ><div></div><div></div></div>
+        <div className={styles["puzzle-cube"]} ><div></div><div></div></div>
+      </div>
+
+      <div className={styles["loading-bar"]}></div>
+    </div>
+  );
+}
+
+export default Loader;
