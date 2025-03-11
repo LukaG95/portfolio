6a95b7aa4901a431281854f7832222bc974c97ff
Ryu <glukec4@gmail.com>
2025-03-11 14:53:40 +0100

    rework social icons

--- client/src/sections/LeftSection.js
+++ client/src/sections/LeftSection.js
@@ -1,16 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './LeftSection.module.scss';
 import { AppContext } from "../context/AppContext"; 
-import Discord from "../images/discord.png";
-import Twitter from "../images/twitter.png";
-import Instagram from "../images/instagram.png";
-import Facebook from "../images/facebook.png";
+import { ReactComponent as Discord } from '../images/discord.svg';
+import { ReactComponent as Twitter } from '../images/twitter.svg';
+import { ReactComponent as Instagram } from '../images/instagram.svg';
+import { ReactComponent as Facebook } from '../images/facebook.svg';
+
 import Profile from "../images/profile.png";
 import Logo from "../images/logo.png";
 import useWindowDimensions from '../WindowDimensions.js';
 
 function LeftSection({ handleNavItemClick }) {
-  const { s_width } = useWindowDimensions();
+  const [openDiscord, setOpenDiscord] = useState(false);
   const { language, color } = useContext(AppContext);
 
   return (
@@ -34,10 +35,35 @@ function LeftSection({ handleNavItemClick }) {
           <div>Ljubljana, SL</div>
         </div>
         <div className={styles.socials}>
-          <div className={styles[`hover-border-${color.name}`]}><img src={Discord} style={{width: 13}}></img></div>
-          <div className={styles[`hover-border-${color.name}`]}><img src={Twitter}></img></div>
-          <div className={styles[`hover-border-${color.name}`]}><img src={Instagram}></img></div>
-          <div className={styles[`hover-border-${color.name}`]}><img src={Facebook}></img></div>
+          <div onMouseEnter={()=> setOpenDiscord(true)} onMouseLeave={()=> setOpenDiscord(false)}> 
+            { openDiscord && <section className={styles["discord-username"]}>ryutrading</section> }
+            <Discord 
+              className={styles.icon} 
+              width={20.67} 
+              height={16.79} 
+            /> 
+          </div>
+          <div onClick={() => window.open("https://x.com/LukaGolob_")}> 
+            <Twitter 
+              className={styles.icon} 
+              width={21} 
+              height={17} 
+            /> 
+          </div>
+          <div onClick={() => window.open("https://www.instagram.com/lukaluxie/")}> 
+            <Instagram 
+              className={styles.icon} 
+              width={20} 
+              height={20} 
+            /> 
+          </div>
+          <div onClick={() => window.open("https://www.facebook.com/luka.golob.35/")}> 
+            <Facebook 
+              className={styles.icon} 
+              width={20} 
+              height={20} 
+            /> 
+          </div>
         </div>
         <div className={`${styles["hire-me-button"]}`} style={{background: color.value, borderBottomColor: color.dark}} onClick={()=> handleNavItemClick(3, "#four")}>
           {language.placeholder_text3}
